3a2c9997aeb08c5ee77a806a7b60dca474938b20
Swieckowski <a.swieckowski@gmail.com>
2017-10-02 16:49:01 -0400

    http request set up

--- ambient.js
+++ ambient.js
@@ -1,17 +1,27 @@
 const tessel = require('tessel');
 const ambientlib = require('ambient-attx4');
-const app = require('express')();
-
-app.listen(3000, () => {
-  console.log('Naked people on 3000');
-});
-
-app.post('/', function(req, res, next){
-
-});
+const http = require('http');
 
 const ambient = ambientlib.use(tessel.port.A);
 let soundCounter = 0;
+
+takePicture.on('data', (image) => {
+  console.log('taking picture');
+
+  const request = http.request({
+    hostname: '172.16.17.53',
+    port: 1337,
+    path: '/pic',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'image/jpg',
+      'Content-Length': image.length
+    }
+  })
+
+  request.write(image)
+
+})
 
 ambient.on('ready', () => {
   // Get points of light and sound data.
@@ -23,7 +33,22 @@ ambient.on('ready', () => {
         if (sounddata.toFixed(8) > 0.4) {
           soundCounter++;
           if (soundCounter === 6) {
-            console.log(`You're too loud.  You're at a ${sounddata.toFixed(3)} and I'm going to need you to bring it down to a 2.`);
+            const db = sounddata.toFixed(3);
+            console.log(`You're too loud.  You're at a ${db} and I'm going to need you to bring it down to a 2.`);
+            const request = http.request({
+              hostname: '172.16.22.120',
+              port: 3000,
+              path: '/',
+              method: 'POST',
+              headers: {
+                        'Content-Type': 'text',
+                        'Content-Length': 1000
+                        }
+            })
+
+            request.write(db)// not sure if db is actually text,
+            // we can change to a string once you've figured 
+            // out what type it actually is
           }
         } else {
           soundCounter = 0;
